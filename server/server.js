6aa4abcba3c162778332ec89cf7d3d08a7ff79d0
mattdean <matthewallandean@gmail.com>
2021-05-07 18:08:55 -0600

    Allow CORS

---
+++ server/server.js
@@ -0,0 +1,16 @@
+const fs = require("fs");
+const httpServer = require("https").createServer({
+  key: fs.readFileSync("/tmp/key.pem"),
+  cert: fs.readFileSync("/tmp/cert.pem")
+});
+
+const io = require("socket.io")(httpServer, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"]
+  }
+});
+
+io.on("connection", socket => { /* ... */ });
+
+httpServer.listen(3000);
