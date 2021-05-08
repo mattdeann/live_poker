5804f72c67d352b7648da10820064ba3bd0f06f8
mattdean <matthewallandean@gmail.com>
2021-05-07 18:26:06 -0600

    Eureka! Connect socket no sweat

--- web/src/App/App.js
+++ web/src/App/App.js
@@ -1,5 +1,14 @@
 import './App.css';
 
+// Socket imports
+import { io } from "socket.io-client";
+const socket = io("http://localhost:8080");
+
+
+socket.on("connect", () => {
+  console.log(socket.id);
+});
+
 function App() {
   return (
     <div className="App">
